b53ad387bd9b0c2327f865b0cf1c9ca819b39632
jmk <jmkolata@gmail.com>
2021-12-25 17:52:12 -0500

    added simple hamburger menu

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var noButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 console.dir(selectPlanButtons);
 
@@ -13,6 +15,20 @@ for (let button of selectPlanButtons) {
 }
 
 noButton.addEventListener('click', () => {
+    closeModal();
+})
+
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    closeModal();
+})
+
+toggleButton.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
+
+function closeModal() {
     modal.style.display = 'none';
     backdrop.style.display = 'none';
-})
+}
