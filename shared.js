42ae14b963c2ff85695105c877e3d11a67aeae6d
jmk <jmkolata@gmail.com>
2021-12-26 22:04:42 -0500

    using js to do some stuff for hamburger menu

--- shared.js
+++ shared.js
@@ -5,30 +5,42 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 
-console.dir(selectPlanButtons);
+console.dir(backdrop);
 
 for (let button of selectPlanButtons) {
     button.addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     })
 }
 
-noButton.addEventListener('click', () => {
-    closeModal();
-})
+if( noButton ) {
+    noButton.addEventListener('click', () => {
+        closeModal();
+    })
+}
 
 backdrop.addEventListener('click', () => {
-    mobileNav.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 })
 
 toggleButton.addEventListener('click', () => {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 })
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    
+    backdrop.classList.remove('open');
 }
