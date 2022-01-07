99cad0137c6fd6b95b3d588519322cb74152d8ec
jmk <jmkolata@gmail.com>
2022-01-07 16:35:58 -0500

    some transitions for the modal and backdrop

--- shared.js
+++ shared.js
@@ -10,9 +10,11 @@ console.dir(backdrop);
 for (let button of selectPlanButtons) {
     button.addEventListener('click', function() {
         // modal.style.display = 'block';
-        // backdrop.style.display = 'block';
+        backdrop.style.display = 'block';
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        setTimeout( () => {
+            backdrop.classList.add("open");
+        }, 10);
     })
 }
 
@@ -30,17 +32,21 @@ backdrop.addEventListener('click', () => {
 
 toggleButton.addEventListener('click', () => {
     // mobileNav.style.display = 'block';
-    // backdrop.style.display = 'block';
+    backdrop.style.display = 'block';
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    setTimeout( () => {
+        backdrop.classList.add("open");
+    }, 10);
 })
 
 function closeModal() {
     // modal.style.display = 'none';
-    // backdrop.style.display = 'none';
+    
     if (modal) {
         modal.classList.remove('open');
     }
-    
-    backdrop.classList.remove('open');
+    backdrop.classList.remove("open")
+    setTimeout( () => {
+        backdrop.style.display = 'none';
+    }, 500);
 }
